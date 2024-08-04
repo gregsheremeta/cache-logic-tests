0dc8288a0674b4e4776b0c7c4579e182c878564e
Greg Sheremeta <gshereme@redhat.com>
2024-08-04 07:31:55 -0400

    clarify how the cache will be used

--- pseudocode-for-checking-cache.go
+++ pseudocode-for-checking-cache.go
@@ -1,13 +1,18 @@
 
 if cache_is_enabled {
 	// try to hit the cache
-	err, got_a_cache_hit := check_cache()
+	err, got_a_cache_hit, cached_results := check_cache()
 	if err != nil {
 		log(“tried to use cache, but hit an error! Will treat as a cache miss and proceed with podSpecPatch.”)
 		// fall through to pod spec patch code below
 	}
 	else if got_a_cache_hit {
 		log(“got a cache hit! Will not do podSpecPatch. Will reuse artifacts from previous result.”)
+
+		// do something with the cache result
+		mark_this_task_as_using_cache()
+		save_artifacts_for_this_task(cached_results)
+
 		return // we are done here! Do NOT fall through to pod spec patch
 	}
 	else {
